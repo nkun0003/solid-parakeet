c3c248296b4e29134b1c1e586d1bd7ba4eaf8ab5
Rocien Nkunga <rociennkunga@gmail.com>
2024-02-16 15:33:42 -0500

    added the pwa img and manifest setting

--- sw.js
+++ sw.js
@@ -22,6 +22,7 @@ self.addEventListener('activate', (ev) => {
 });
 self.addEventListener('fetch', (ev) => {
   //try the cache first, then fetch and save copy in cache
+  ev.respondWith(cacheFirstAndSave(ev));
 });
 
 function cacheFirst(ev) {
@@ -32,6 +33,22 @@ function cacheFirst(ev) {
 }
 function cacheFirstAndSave(ev) {
   //try cache then fetch
+  return caches.match(ev.request).then((cacheResponse) => {
+    return cacheResponse || 
+    fetch(ev.request).then(fetchResp => {
+      if(fetchResp.status > 0 && !fetchResp.ok) throw new Error('bad response');
+      // throw the error IF NOT opaque AND NOT in the 200-299 range
+      // we will just return a 404 error... to keep it simple
+      return caches.open(cacheName).then(cache=>{
+        return cache.put(ev.request, fetchResp.clone());
+      }).then(()=>{
+        //finished putting the copy in the cache
+        return fetchResp; //send the resp to the browser
+      })
+    }).catch(err=>{
+      return response404();
+    });
+  })
 }
 function response404() {
   //any generic 404 error that we want to generate
